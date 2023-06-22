a3dd86bbe307d17b53e677cbef6a576f43c422b9
Prabhudatta Giri <113381845+prabhudattagiri@users.noreply.github.com>
2023-06-22 20:39:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#element", {
+  strings: [
+    "Data Scientist",
+    "AI/ML Engineer",
+    "Web Designer",
+    "Content Creator",
+    "Web Developer",
+  ],
+  typeSpeed: 50,
+});
